9eacf650d2bf36f457adae6a0b34cf5aa4246e9e
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2021-02-23 15:11:09 +0000

    Update 2_functional-tests.js

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -5,7 +5,6 @@ const server = require('../server.js');
 
 chai.use(chaiHttp);
 
-let Translator = require('../components/translator.js');
 
 suite('Functional Tests', () => {
 suite("With text fields",() => {
